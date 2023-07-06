4f28cbabc309caf6759b89533fbf9c988d57f4f9
Felipe Miranda <felipe.miranda@stcmed.com>
2023-07-06 16:29:52 +0200

    EpochSaver callback added and used in jupyter notebook

--- callbacks.py
+++ callbacks.py
@@ -1,14 +1,20 @@
+import os
 import tensorflow as tf
+import pathlib
 
 
-class SaveModelOnEpoch(tf.keras.callbacks.Callback):
-    def __init__(self, base_name, st=False):
+class EpochSaver(tf.keras.callbacks.Callback):
+    def __init__(self, layer_name, model_path, base_name):
+        self.layer_name = layer_name
+        self.model_path = model_path
         self.base_name = base_name
-        self.st = st
         super().__init__()
 
     def on_epoch_end(self, epoch, logs={}):
-        if self.st:
-            self.model.layers[1].save(self.base_name + "_e" + str(epoch + 1) + ".h5")
-        else:
-            self.model.save(self.base_name + "_e" + str(epoch + 1) + ".h5")
+        if not (epoch + 1) % 1 == 0:
+            return
+        pathlib.Path(self.model_path).mkdir(parents=True, exist_ok=True)
+        path = os.path.join(self.model_path, self.base_name) + "_e" + str(epoch + 1) + ".h5"
+        self.model.get_layer(self.layer_name).save(path)
+        print("class weights saved to path: ")
+        print(path)
